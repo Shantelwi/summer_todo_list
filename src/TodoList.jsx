b968a83d2adf859f26bd9669faa7ec6871a0cc93
Shantel Williams <williamsshantel73@gmail.com>
2026-07-15 20:54:45 +0000

    fixed bugs

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,17 @@
 import TodoListItem from "./TodoListItem.jsx";
 
 function TodoList({ todoList }) {
+    const todos = [
+  { id: 1, title: "review resources" },
+  { id: 2, title: "take notes" },
+  { id: 3, title: "code out app" }
+]
     return (
+
         <ul>
             {todoList.map((todo) => {
                 return (
-                    <TodoListItem key={todo.id} todo={todo}
-                    />
+                    <li key={todo.id}>{todo.title}</li>
                 )
                 })}
         </ul>
