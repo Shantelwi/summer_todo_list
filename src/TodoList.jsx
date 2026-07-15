00c38856b953ad5687c645d74d03435b1dceba39
Shantel Williams <williamsshantel73@gmail.com>
2026-07-15 15:36:00 +0000

    fixed errors

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,7 +3,7 @@ import TodoListItem from "./TodoListItem.jsx";
 function TodoList({todoList}) {
     return (
         <ul>
-            {todoList.map(todo => <TodoListItem key={todo.id}/>)}
+            {todoList.map(todo => <TodoListItem key={todo.id} prop={todo.title}/>)}
         </ul>
     );
 }
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -4,9 +4,10 @@ function TodoListItem() {
     const [todo, setTodo] = useState();
     
     return(
-        <li>
+       <>
             {todoList.map(todo => {todo.title})}
-        </li>
+       </>
+        
     )
 }
 
