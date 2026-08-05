d3588fdbac8b8c2c0796b22a9f19f649705a141f
Shantel Williams <williamsshantel73@gmail.com>
2026-08-05 15:04:27 +0000

    finished part 3

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,28 +1,31 @@
-import {useRef} from 'react';
+import { useRef, useState } from 'react';
+
 
 function TodoForm({ onAddTodo }) {
+    //add local state 
+    const [workingTodoTitle, setWorkingTodoTitle] = useState("");
     const inputRef = useRef();
 
     const handleAddTodo = (e) => {
         e.preventDefault();
-
-        //.trim prevents whitespace only todos
-        const todoTitle = e.target.todoTitle.value.trim();
-        if (todoTitle && todoTitle !== "") {
-            onAddTodo(todoTitle);
-            e.target.reset();
-            inputRef.current.focus();
-        }
+        onAddTodo(workingTodoTitle);
+        setWorkingTodoTitle("");
+        inputRef.current.focus();
     };
+
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
-            <input 
-                ref = {inputRef}
-                type="text" 
+            <input
+                ref={inputRef}
+                type="text"
                 id="todoTitle"
                 name='todoTitle'
                 placeholder={'Todo Text'}
+                //add a value prop to the input element, setting it to workingTodoTitle
+                value={workingTodoTitle}
+                //add an onChange event handler to the input that takes the event object as a parameter then calls the state setter function with e.target.value
+                onChange = {(e) => {setWorkingTodoTitle(e.target.value)} }
                 required
             />
             <button type="submit">Add Todo</button>
