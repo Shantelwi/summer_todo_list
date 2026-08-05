9e50b788936d1d8f19a70cb0148d6784744d4827
Shantel Williams <williamsshantel73@gmail.com>
2026-08-05 15:08:26 +0000

    finished part 4

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -28,7 +28,7 @@ function TodoForm({ onAddTodo }) {
                 onChange = {(e) => {setWorkingTodoTitle(e.target.value)} }
                 required
             />
-            <button type="submit">Add Todo</button>
+            <button type="submit" disabled={!workingTodoTitle.trim()} >Add Todo</button> {/* add a disabled prop. set the disabled prop to true when workingTodoTitle is an empty string or contains only whitespace. */}
         </form>
     );
 }
