8b8daec7b31d46306eafb1961004e89aac9d66df
Shantel Williams <williamsshantel73@gmail.com>
2026-08-13 15:21:25 +0000

    refactor input and label into a re-usable component

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import TextInputWithLabel from '../shared/TextInputWithLabel.jsx';
 
 
 function TodoForm({ onAddTodo }) {
@@ -15,19 +16,23 @@ function TodoForm({ onAddTodo }) {
 
     return (
         <form onSubmit={handleAddTodo}>
-            <label htmlFor="todoTitle">Todo</label>
-            <input
+            <TextInputWithLabel 
                 ref={inputRef}
-                type="text"
-                id="todoTitle"
-                name='todoTitle'
-                placeholder={'Todo Text'}
                 //add a value prop to the input element, setting it to workingTodoTitle
                 value={workingTodoTitle}
                 //add an onChange event handler to the input that takes the event object as a parameter then calls the state setter function with e.target.value
                 onChange = {(e) => {setWorkingTodoTitle(e.target.value)} }
-                required
+                elementId = 'todoTitle'
+                labelText = 'Todo'
             />
+            {/* <label htmlFor="todoTitle">Todo</label>
+            <input
+                type="text"
+                id="todoTitle"
+                name='todoTitle'
+                placeholder={'Todo Text'}
+                required
+            /> */}
             <button type="submit" disabled={!workingTodoTitle.trim()} >Add Todo</button> {/* add a disabled prop. set the disabled prop to true when workingTodoTitle is an empty string or contains only whitespace. */}
         </form>
     );
