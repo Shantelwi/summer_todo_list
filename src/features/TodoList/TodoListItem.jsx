5414359531eada3921ddfa9112c01038358a9e90
Shantel Williams <williamsshantel73@gmail.com>
2026-08-14 02:10:48 +0000

    update todo state from local state

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,7 +1,8 @@
 import TextInputWithLabel from '../../shared/TextInputWithLabel.jsx';
 import { useState } from 'react';
+import { isValidTodoTitle } from '../../utils/todoValidation.js';
 
-function TodoListItem({ todo, onCompleteTodo }) {
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
 
     const [isEditing, setIsEditing] = useState(false);
     const [workingTitle, setWorkingTitle] = useState(todo.title);
@@ -15,14 +16,28 @@ function TodoListItem({ todo, onCompleteTodo }) {
         setWorkingTitle(event.target.value);
     }
 
+    function handleUpdate(event){
+        if (!isEditing) {
+            return;
+        }
+        event.preventDefault();
+
+        if (!isValidTodoTitle(workingTitle)) {
+            return;
+        }
+
+        onUpdateTodo({...todo, title: workingTitle});
+        setIsEditing(false);
+    }
 
     return (
         <li>
-            <form>
+            <form onSubmit={handleUpdate}>
                 {isEditing ? (
                     <>
                         <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
                         <button type='button' onClick={handleCancel}>Cancel</button>
+                        <button type='button' onClick={handleUpdate}>Update</button>
                     </>
                 ) : (
                     <>
